55a8771500f62b04fa00fa1d1fe47344ecd0455f
jordy-torvalds <jordy-torvalds@kakao.com>
2021-03-06 16:07:33 +0900

    initailize: 프로젝트 초기 세팅

---
+++ src/main/java/com/jordytech/exchangeratecalculate/ExchangeRateCalculateApplication.java
@@ -0,0 +1,13 @@
+package com.jordytech.exchangeratecalculate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateCalculateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateCalculateApplication.class, args);
+	}
+
+}
