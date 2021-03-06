4f83e51ac0267630645679920689be553aba3632
jordy-torvalds <jordy-torvalds@kakao.com>
2021-03-06 16:19:36 +0900

    project-modify : 패키지명과 pom.xml 프로젝트 메타 데이터 변경

--- src/test/java/com/jordy/app/exchangeratecalculrate/ExchangeRateCalculateApplicationTests.java
+++ src/test/java/com/jordy/app/exchangeratecalculrate/ExchangeRateCalculateApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jordytech.exchangeratecalculate;
+package com.jordy.app.exchangeratecalculrate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
