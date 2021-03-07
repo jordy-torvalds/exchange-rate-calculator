18ac6b04a0a17652179e7e821aaa428d403cc67c
jordy-torvalds <jordy-torvalds@kakao.com>
2021-03-07 18:03:32 +0900

    test(exchange-rate): 환율 조회 클라이언트 조회 정상 여부 확인 테스트 추가

--- src/test/java/com/jordy/app/exchangeratecalculrate/currency/infra/ApiExchangeRateClientTest.java
+++ src/test/java/com/jordy/app/exchangeratecalculrate/currency/infra/ApiExchangeRateClientTest.java
@@ -1,14 +1,22 @@
 package com.jordy.app.exchangeratecalculrate.currency.infra;
 
+import com.jordy.app.exchangeratecalculrate.currency.dto.ExchangeRateResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.io.UnsupportedEncodingException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Slf4j
 @SpringBootTest
 @ActiveProfiles("prod")
 class ApiExchangeRateClientTest {
@@ -17,10 +25,25 @@ class ApiExchangeRateClientTest {
     ExchangeRateClient exchangeRateClient;
 
     @Test
-    @DisplayName("운영(prod) 프로필일 때 ApiExchnageRateClient가 활성화되는지 확인")
+    @DisplayName("운영(prod) 프로필일 때 ApiExchangeRateClient가 활성화되는지 확인")
     public void checkApiExchangeRateClient(){
-        assertEquals(exchangeRateClient.getClass(), ApiExchangeRateClient.class);
+        assertEquals(ApiExchangeRateClient.class, exchangeRateClient.getClass());
     }
 
 
+    @Test
+    @DisplayName("ApiExchangeRateClient로 정상적으로 조회되는지 확인")
+    void checkCommunicationWithExchangeRateServer() throws UnsupportedEncodingException{
+
+        // Given (skip)
+        ApiExchangeRateClient apiExchangeRateClient = (ApiExchangeRateClient) exchangeRateClient;
+
+        // When
+        ResponseEntity<ExchangeRateResponse> responseEntity = apiExchangeRateClient.requestToCurrencyLayer();
+
+        // Then
+        assertThat(responseEntity.getBody().isSuccess()).isTrue();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        log.info(responseEntity.getBody().toString());
+    }
 }
