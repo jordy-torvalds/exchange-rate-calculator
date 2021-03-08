f82bc479750c83d9095426fb82be38b4fd45404c
jordy-torvalds <jordy-torvalds@kakao.com>
2021-03-08 10:39:58 +0900

    feat (currency) : 환율 조회 로직 구현

---
+++ src/main/java/com/jordy/app/exchangeratecalculrate/currency/application/CurrencyService.java
@@ -0,0 +1,45 @@
+package com.jordy.app.exchangeratecalculrate.currency.application;
+
+import com.jordy.app.exchangeratecalculrate.currency.dto.CurrencyRequest;
+import com.jordy.app.exchangeratecalculrate.currency.dto.CurrencyResponse;
+import com.jordy.app.exchangeratecalculrate.currency.dto.ExchangeRateResponse;
+import com.jordy.app.exchangeratecalculrate.currency.infra.ExchangeRateClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CurrencyService {
+
+    final ExchangeRateClient exchangeRateClient;
+
+    public CurrencyResponse getCalculatedCurrency() throws UnsupportedEncodingException {
+        ExchangeRateResponse exchangeRateResponse = exchangeRateClient.retrieveExchangeRate();
+
+
+        return null;
+    }
+
+    private ExchangeRateResponse calculateCurrencyWithExchangeRate(CurrencyRequest currencyRequest) {
+        ExchangeRateResponse exchangeRateResponse = exchangeRateClient.retrieveExchangeRate();
+
+        return null;
+    }
+
+    public CurrencyResponse retrieveExchangeRate(String currencySpecies) {
+        ExchangeRateResponse exchangeRateResponse = exchangeRateClient.retrieveExchangeRate();
+        BigDecimal exchangeRate = exchangeRateResponse.retrieveExchangeRate(currencySpecies);
+
+        if(exchangeRateResponse.isSuccess() == false)
+            return new CurrencyResponse();
+        else if ( exchangeRate.equals(BigDecimal.ZERO))
+            return new CurrencyResponse(false, exchangeRate);
+
+        return new CurrencyResponse(true, exchangeRateResponse.retrieveExchangeRate(currencySpecies));
+    }
+}
